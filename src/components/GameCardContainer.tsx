45280c2a5b43b689ebbdfb3ce476359b743ed50b
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2024-08-31 00:05:28 +0530

    Added code to displayed the genre

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,12 +7,7 @@ interface GameCardContainerProps {
 
 const GameCardContainer = ({ children }: GameCardContainerProps) => {
   return (
-    <Box
-      borderRadius={10}
-      overflow={"hidden"}
-      paddingBottom={5}
-      width={"300px"}
-    >
+    <Box borderRadius={10} overflow={"hidden"} paddingBottom={5}>
       {children}
     </Box>
   );
